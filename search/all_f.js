b17d9fae93ca1ff2e2791b604a04af1de7c421ae
whs31 <75864292+whs31@users.noreply.github.com>
2024-06-19 15:56:14 +0000

    Deploying to gh-pages from @ tmp-name-org/floppy@379719dce0e944192b31ea7b5bb559b78e4f25b0 🚀

--- search/all_f.js
+++ search/all_f.js
@@ -21,7 +21,7 @@ var searchData=
   ['preference_5fdir_18',['preference_dir',['../d6/d2d/classfloppy_1_1filesystem_1_1application__dirs.html#abf7464131f356ab780da1279c200b0be',1,'floppy::filesystem::application_dirs']]],
   ['preferences_19',['preferences',['../d6/d2d/classfloppy_1_1filesystem_1_1application__dirs.html#aa671c41b56810ab4bfd94587e8507a75a1ce027fe3518cc64ef3570b50a9a7c10',1,'floppy::filesystem::application_dirs']]],
   ['project_20',['project',['../d4/d33/classfloppy_1_1math_1_1vector2d.html#ae0b17b38d47866c22c3b1ef76a7081f2',1,'floppy::math::vector2d']]],
-  ['project_5fmeta_21',['project_meta',['../d2/de7/classfloppy_1_1meta_1_1project__meta.html',1,'project_meta'],['../d2/de7/classfloppy_1_1meta_1_1project__meta.html#a9d1060ccbe8a77714b3735df9bba121e',1,'floppy::meta::project_meta::project_meta()']]],
+  ['project_5fmeta_21',['project_meta',['../d2/de7/classfloppy_1_1meta_1_1project__meta.html',1,'project_meta'],['../d2/de7/classfloppy_1_1meta_1_1project__meta.html#aa78e91b1785f8ee1dce291227879dc9a',1,'floppy::meta::project_meta::project_meta()']]],
   ['project_5fpath_22',['project_path',['../d6/d2d/classfloppy_1_1filesystem_1_1application__dirs.html#aec57925e9296d77eff2481f636d1933b',1,'floppy::filesystem::application_dirs']]],
   ['ptr_23',['ptr',['../d9/d8a/structfloppy_1_1traits_1_1singleton.html#a3668e49f287d02953577eeb995ab64e2',1,'floppy::traits::singleton']]],
   ['ptr_5fmut_24',['ptr_mut',['../d9/d8a/structfloppy_1_1traits_1_1singleton.html#afce897b3503cc4e00c72624f156c04d2',1,'floppy::traits::singleton']]],
--- search/functions_d.js
+++ search/functions_d.js
@@ -8,7 +8,7 @@ var searchData=
   ['positive_5',['positive',['../de/d4d/structfloppy_1_1math_1_1angle.html#ac4bf6b495d5787265c5f51db4fe5f13d',1,'floppy::math::angle']]],
   ['preference_5fdir_6',['preference_dir',['../d6/d2d/classfloppy_1_1filesystem_1_1application__dirs.html#abf7464131f356ab780da1279c200b0be',1,'floppy::filesystem::application_dirs']]],
   ['project_7',['project',['../d4/d33/classfloppy_1_1math_1_1vector2d.html#ae0b17b38d47866c22c3b1ef76a7081f2',1,'floppy::math::vector2d']]],
-  ['project_5fmeta_8',['project_meta',['../d2/de7/classfloppy_1_1meta_1_1project__meta.html#a9d1060ccbe8a77714b3735df9bba121e',1,'floppy::meta::project_meta']]],
+  ['project_5fmeta_8',['project_meta',['../d2/de7/classfloppy_1_1meta_1_1project__meta.html#aa78e91b1785f8ee1dce291227879dc9a',1,'floppy::meta::project_meta']]],
   ['project_5fpath_9',['project_path',['../d6/d2d/classfloppy_1_1filesystem_1_1application__dirs.html#aec57925e9296d77eff2481f636d1933b',1,'floppy::filesystem::application_dirs']]],
   ['ptr_10',['ptr',['../d9/d8a/structfloppy_1_1traits_1_1singleton.html#a3668e49f287d02953577eeb995ab64e2',1,'floppy::traits::singleton']]],
   ['ptr_5fmut_11',['ptr_mut',['../d9/d8a/structfloppy_1_1traits_1_1singleton.html#afce897b3503cc4e00c72624f156c04d2',1,'floppy::traits::singleton']]]
