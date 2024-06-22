d71d36e99d744bc4f2f6dc10dbfd755584f614d9
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-06-22 17:13:07 +0300

    Platform refactoring, formatters, target triple string

--- include/floppy/floppy.h
+++ include/floppy/floppy.h
@@ -11,3 +11,21 @@
 #include <floppy/detail/print_helpers.h>
 #include <floppy/detail/type_traits.h>
 #include <floppy/detail/types.h>
+
+/// \brief Formatter for `floppy::platform::vendor`.
+template <> struct [[maybe_unused]] fmt::formatter<floppy::platform::vendor> : floppy::ostream_formatter<char> {};
+
+/// \brief Formatter for `floppy::platform::operating_system`.
+template <> struct [[maybe_unused]] fmt::formatter<enum floppy::platform::operating_system> : floppy::ostream_formatter<char> {};
+
+/// \brief Formatter for `floppy::platform::arch`.
+template <> struct [[maybe_unused]] fmt::formatter<floppy::platform::arch> : floppy::ostream_formatter<char> {};
+
+/// \brief Formatter for `floppy::platform::compiler`.
+template <> struct [[maybe_unused]] fmt::formatter<enum floppy::platform::compiler> : floppy::ostream_formatter<char> {};
+
+/// \brief Formatter for `floppy::platform::endianness`.
+template <> struct [[maybe_unused]] fmt::formatter<enum floppy::platform::endianness> : floppy::ostream_formatter<char> {};
+
+/// \brief Formatter for `floppy::platform`.
+template <> struct [[maybe_unused]] fmt::formatter<floppy::platform> : floppy::ostream_formatter<char> {};
