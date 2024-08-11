b82a897bc103e05f2d13b0a5cb4733899d8a2952
whs31 <40-ryazancev_dl@users.noreply.gitlab.radar076.ru>
2024-08-11 22:59:28 +0300

    Minor fix

--- include/floppy/backtrace/file.h
+++ include/floppy/backtrace/file.h
@@ -6,7 +6,7 @@
 
 namespace floppy::stacktrace::impl
 {
-  [[maybe_unused]] [[nodiscard]] auto make_crash_report_path(std::filesystem::path const& folder = std::filesystem::current_path() / "crashdumps")
+  [[maybe_unused]] [[nodiscard]] auto make_crash_report_path(std::filesystem::path const& folder = std::filesystem::current_path() / "crash_reports")
     -> std::filesystem::path {
     if(not std::filesystem::exists(folder))
       std::filesystem::create_directories(folder);
@@ -14,6 +14,6 @@ namespace floppy::stacktrace::impl
     auto time = std::chrono::system_clock::to_time_t(current_time);
     auto ss = std::stringstream();
     ss << std::put_time(std::localtime(&time), "%Y-%m-%d_%H-%M-%S");
-    return folder / ("crash_reports_" + ss.str() + ".log");
+    return folder / ("crash_" + ss.str() + ".log");
   }
 } // namespace floppy::stacktrace::impl
